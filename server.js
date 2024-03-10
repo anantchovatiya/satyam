3f54a789eb332c6b5ce280caebfe5c491452f5f3
anantchovatiya <147299670+anantchovatiya@users.noreply.github.com>
2024-03-10 16:04:50 +0530

    Update server.js

--- server.js
+++ server.js
@@ -67,7 +67,7 @@ app.post("/dcode", (req, res) => {
                     udis: ucode
                 });
             } else {
-                res.send("Incorrect Dealer Code");
+                res.render("err");
             }
         }
 
@@ -75,7 +75,9 @@ app.post("/dcode", (req, res) => {
 
 
 })
-
+app.post("/back", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/main.html"));
+})
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
